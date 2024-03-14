d36dacef75ebdc2b4a005743ab8d601e3b21852f
Your Name <you@example.com>
2024-03-14 00:16:00 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -66,9 +66,6 @@ arreglos en un tercer arreglo también ordenado.
 */
 void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,int result[]) 
 {
-  int *size3 = 0;
-  int *arr3 = (int *) realloc(arr3, *size3 * sizeof(int));
-
   int tallaMayor = 0;
   if (size1 > size2) tallaMayor = size1;
   else tallaMayor = size2;
